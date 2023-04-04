e8314298c43de44f9a6e647f9b3f5860448fa559
Unies Ananda Raja <uanandaraja@gmail.com>
2023-04-04 11:57:44 +0700

    first commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const meta = {
-    title: 'Next.js Blog Starter Kit',
-    description: 'Clone and deploy your own Next.js portfolio in minutes.',
+    title: 'Unies Ananda Raja',
+    description: 'Blog and portfolio of Unies Ananda Raja',
     image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png',
   }
 
---
+++ pages/blog/index.md
@@ -0,0 +1,7 @@
+---
+type: posts
+title: Blog
+date: 2023-04-04
+---
+
+# Posts

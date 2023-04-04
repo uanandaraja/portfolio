70f951ed68edabd5090e1d867701260e2e2610a4
Unies Ananda Raja <uanandaraja@gmail.com>
2023-04-04 22:33:01 +0700

    minor changes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   const meta = {
     title: 'Unies Ananda Raja',
     description: 'Blog and portfolio of Unies Ananda Raja',
-    image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png',
+    image: '/images/comingofagi.png',
   }
 
   return (
